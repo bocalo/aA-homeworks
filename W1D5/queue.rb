05bdd191dfd96b4c966740043e205049ee340ae8
bocalo <moldavskiym@yandex.ru>
2020-02-26 08:48:38 +0300

    Add methods in queue file

---
+++ W1D5/queue.rb
@@ -0,0 +1,23 @@
+
+class Queue
+    def initialize
+        @underlying_array = []
+    end
+
+    def enqueue(el)
+        underlying_array.push(el)
+        el
+    end
+
+    def dequeue
+        underlying_array.shift
+    end
+
+    def peek
+        underlying_array.first
+    end
+
+    private 
+
+    attr_reader :underlying_array
+end
